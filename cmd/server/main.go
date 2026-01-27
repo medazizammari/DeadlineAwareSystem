461de6ee422e98b2a08d4eeb065ce2024aa435b3
Med Aziz Ammari <50046193+medazizammari@users.noreply.github.com>
2026-01-27 13:49:11 +0000

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// TODO: load config
+	// TODO: build app (router, services)
+	// TODO: start server in goroutine
+
+	<-ctx.Done()
+	log.Println("shutdown signal received")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	_ = shutdownCtx
+	// TODO: gracefully shutdown HTTP server, close resources
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/medazizammari/real-time-deadline-aware-golang
+
+go 1.25.6
